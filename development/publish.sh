911fb11fd94ed5cfc5400e15f4d471e07604433d
Evan Summers <evan.summers@gmail.com>
2017-02-18 13:58:43 +0000

    publish

--- development/publish.sh
+++ development/publish.sh
@@ -1,6 +1,8 @@
 
-  npm version | head -1
-  npm info | grep latest
+set -x 
+
+  version=`npm version | head -1 | sed "s/.*'\([0-9].*\)',/\1/"`
+  npm info | grep latest | grep -v $version
 
   git add -A
   git commit -m 'publish'
