c39d67419a0bbe879ac00d11f2ac0eb8a1487014
Evan Summers <evan.summers@gmail.com>
2017-02-25 13:20:09 +0000

    publish

--- bin/docker-inspect-ip.sh
+++ bin/docker-inspect-ip.sh
@@ -4,4 +4,4 @@ set -u -e
   [ $# -eq 1 ]
 
   docker inspect $1 |
-      grep '"IPAddress":' | tail -1 | sed 's/.*"\([0-9\.]*\)",/\1/'`
+      grep '"IPAddress":' | tail -1 | sed 's/.*"\([0-9\.]*\)",/\1/'
