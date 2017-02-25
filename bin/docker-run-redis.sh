0dc9fda90c1ccf24cc3794eac57dca01739ae796
Evan Summers <evan.summers@gmail.com>
2017-02-25 13:37:54 +0000

    publish

--- bin/docker-run-redis.sh
+++ bin/docker-run-redis.sh
@@ -3,6 +3,13 @@ set -u -e
 
   [ $# -eq 1 ]
   name="$1"
-
-  docker run --network=$name-network --name $name-redis -d redis
+  network=${name}-network 
+  docker network ls -q -f name=^$network | grep -vq '\w' && 
+    docker network create -d bridge $network
+  docker inspect `
+    docker run --network=$network --name $name-redis -d redis
+  ` |
+    grep '"IPAddress":' | 
+    tail -1 | 
+    sed 's/.*"\([0-9\.]*\)",/\1/'`
 
