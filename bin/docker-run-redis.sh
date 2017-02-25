5151cef178f392c789e95d36dd6a13964d27a86f
Evan Summers <evan.summers@gmail.com>
2017-02-25 13:54:44 +0000

    publish

--- bin/docker-run-redis.sh
+++ bin/docker-run-redis.sh
@@ -5,15 +5,14 @@ set -u -e
   name="$1"
   network="${name}-network"
 
-  container=`docker ps -a -q -f "name=/${name}-redis"`
-  if [ -n "$container" ]
+  if docker network ls -q -f name=^$network | wc -l | grep -q '^0$' 
   then
-    echo "Already exists: $container"
-  else
-    if docker network ls -q -f name=^$network | wc -l | grep -q '^0$' 
-    then
-      docker network create -d bridge $network
-    fi
+    docker network create -d bridge $network >/dev/null
+  fi
+
+  container=`docker ps -a -q -f "name=/${name}-redis"`
+  if [ -z "$container" ]
+  then
     container=`docker run --network=$network --name ${name}-redis -d redis`
   fi
   docker inspect "$container" |
