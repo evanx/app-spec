96ec36952aae7edf5a987543be5ca998a2c1e5f6
Evan Summers <evan.summers@gmail.com>
2017-02-25 13:52:11 +0000

    publish

--- bin/docker-run-redis.sh
+++ bin/docker-run-redis.sh
@@ -4,11 +4,19 @@ set -u -e
   [ $# -eq 1 ]
   name="$1"
   network="${name}-network"
-  docker network ls -q -f name=^$network | wc -l | grep -q '^0$' && 
-    docker network create -d bridge $network
-  docker inspect `
-    docker run --network=$network --name $name-redis -d redis
-  ` |
+
+  container=`docker ps -a -q -f "name=/${name}-redis"`
+  if [ -n "$container" ]
+  then
+    echo "Already exists: $container"
+  else
+    if docker network ls -q -f name=^$network | wc -l | grep -q '^0$' 
+    then
+      docker network create -d bridge $network
+    fi
+    container=`docker run --network=$network --name ${name}-redis -d redis`
+  fi
+  docker inspect "$container" |
     grep '"IPAddress":' | 
     tail -1 | 
     sed 's/.*"\([0-9\.]*\)",/\1/'
