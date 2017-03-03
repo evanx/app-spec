30f0446f8730907a601c272d65060d07abfbb76b
Evan Summers <evan.summers@gmail.com>
2017-03-03 00:56:48 +0000

    publish

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const mapMetas = metas => Object.keys(metas).map(key => {
         const envDefault = meta.defaults[process.env.NODE_ENV];
         if (envDefault) {
             if (meta.default) {
-                console.error(clc.yellow(`Overriding ${key} to '${envDefault}'`));
+                //console.error(clc.yellow(`Overriding ${key} to '${envDefault}'`));
             }
             meta.default = envDefault;
         }
